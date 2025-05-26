ef63456c7a2121a176ad8f9d9737dc5f54feb028
KiraRozkuliak <s31647@pjwstk.edu.pl>
2025-05-26 15:31:13 +0200

    26.05

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+
     public float moveSpeed = 5;
     public float runSpeed = 7;
     public float jumpForce = 300;
-
- 
     public Rigidbody2D rb;
-  
+    public GroundCheck groundCheck;
 
+
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -21,21 +22,27 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveInput =Input.GetAxis("Horizontal");
+        float moveInput = Input.GetAxis("Horizontal");
         //Debug.Log($"Input value: {moveInput}");
         //rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
         if (Input.GetKey(KeyCode.LeftShift))
         {
             rb.velocity = new Vector2(moveInput * runSpeed, rb.velocity.y);
         }
-        else {
-            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
-        }
-        
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        else
+
         {
-            rb.AddForce(new Vector2(0, jumpForce));
+            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && groundCheck.isGrounded)
+        {
+            //rb.AddForce(new Vector2(0,jumpForce));
+            rb.AddForce(Vector2.up * jumpForce);
+
+        }
+
     }
 }
